edc07d26d9c4a6b2c2c0d7c900deb1eabd968c34
wx19880921 <694114036@qq.com>
2015-10-26 01:05:54 +0800

    first commit

---
+++ files/home/data.js
@@ -0,0 +1,49 @@
+﻿$axure.loadCurrentPage({
+  "url":"home.html",
+  "generationDate":new Date(1445783304845.04),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable",
+"CommentText"],
+  "page":{
+    "packageId":"0dd9fb22ec154b9b8e3228fa2c59f7af",
+    "type":"Axure:Page",
+    "name":"Home",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+      "onLoad":{
+        "description":"OnLoad",
+        "cases":[{
+            "description":"用例 1",
+            "isNewIfGroup":false,
+            "actions":[{
+                "action":"linkWindow",
+                "description":"在 当前窗口 打开 等待",
+                "target":{
+                  "targetType":"page",
+                  "url":"等待.html",
+                  "includeVariables":true},
+                "linkType":"current"}]}]}},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
---
+++ files/等待/data.js
@@ -0,0 +1,151 @@
+﻿$axure.loadCurrentPage({
+  "url":"等待.html",
+  "generationDate":new Date(1445783306528.14),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable",
+"CommentText"],
+  "page":{
+    "packageId":"47df2fec3bc644bb86c096646ea7de1c",
+    "type":"Axure:Page",
+    "name":"等待",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+      "onLoad":{
+        "description":"OnLoad",
+        "cases":[{
+            "description":"用例 1",
+            "isNewIfGroup":false,
+            "actions":[{
+                "action":"wait",
+                "description":"等待3000毫秒",
+                "waitTime":3000},
+{
+                "action":"linkWindow",
+                "description":"在 当前窗口 打开 登陆",
+                "target":{
+                  "targetType":"page",
+                  "url":"登陆.html",
+                  "includeVariables":true},
+                "linkType":"current"}]}]}},
+    "diagram":{
+      "objects":[{
+          "id":"75fa82e67a6d4903b569503c9ba00979",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "size":{
+              "width":375,
+              "height":647}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"259d834ecdef4cef850a52718c785962",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "size":{
+                  "width":375,
+                  "height":647}},
+              "adaptiveStyles":{
+}}],
+          "interactionMap":{
+            "onClick":{
+              "description":"OnClick",
+              "cases":[{
+                  "description":"用例 1",
+                  "isNewIfGroup":false,
+                  "actions":[{
+                      "action":"linkWindow",
+                      "description":"在 当前窗口 打开 登陆",
+                      "target":{
+                        "targetType":"page",
+                        "url":"登陆.html",
+                        "includeVariables":true},
+                      "linkType":"current"}]}]}},
+          "tabbable":true,
+          "images":{
+            "normal~":"images/等待/u0.jpg"}},
+{
+          "id":"869de1dc8dca49918739923c03692efb",
+          "label":"",
+          "type":"textBox",
+          "styleType":"textBox",
+          "visible":true,
+          "style":{
+            "fontName":"'方正舒体 Bold', '方正舒体'",
+            "fontSize":"20px",
+            "fontWeight":"700",
+            "foreGroundFill":{
+              "fillType":"solid",
+              "color":0xFF0000FF,
+              "opacity":1},
+            "fill":{
+              "fillType":"solid",
+              "color":0xFFFFFF},
+            "location":{
+              "x":104,
+              "y":117},
+            "size":{
+              "width":168,
+              "height":33}},
+          "adaptiveStyles":{
+}},
+{
+          "id":"46e3518698ee465e841b57220835b1de",
+          "label":"",
+          "type":"textBox",
+          "styleType":"textBox",
+          "visible":true,
+          "style":{
+            "fontName":"'方正舒体 Bold', '方正舒体'",
+            "fontSize":"20px",
+            "fontWeight":"700",
+            "foreGroundFill":{
+              "fillType":"solid",
+              "color":0xFF0000FF,
+              "opacity":1},
+            "fill":{
+              "fillType":"solid",
+              "color":0xFFFFFF},
+            "location":{
+              "x":104,
+              "y":164},
+            "size":{
+              "width":168,
+              "height":33}},
+          "adaptiveStyles":{
+}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "75fa82e67a6d4903b569503c9ba00979":{
+      "scriptId":"u0"},
+    "259d834ecdef4cef850a52718c785962":{
+      "scriptId":"u1"},
+    "869de1dc8dca49918739923c03692efb":{
+      "scriptId":"u2"},
+    "46e3518698ee465e841b57220835b1de":{
+      "scriptId":"u3"}}});
